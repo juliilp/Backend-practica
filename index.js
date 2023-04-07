39c4c881ad0589c9e1b579d941a401fcf0d6bd04
juliilp <105088200+juliilp@users.noreply.github.com>
2023-04-07 13:02:54 -0300

    deploy

--- index.js
+++ index.js
@@ -1,15 +1,17 @@
 // Importar Express
 const express = require("express");
-
+const bodyparse = require("body-parser");
 // Crear una instancia de la aplicación
 const app = express();
 
+app.use(bodyparse.urlencoded({ extended: true }));
+app.use(bodyparse.json());
 // Definir una ruta con la función controladora
 app.get("/", function (req, res) {
   res.send("Hola mundo!");
 });
-
+const PORT = process.env.PORT || 3001;
 // Levantar el servidor
-app.listen(3003, function () {
-  console.log("Servidor levantado en el puerto 3000");
+app.listen(PORT, function () {
+  console.log("Servidor levantado en el puerto 3001");
 });
